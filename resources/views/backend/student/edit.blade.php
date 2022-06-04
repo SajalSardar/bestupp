f4bb9b5c19306ca7025c7430d90110e1b250c5e9
Sajal Sardar <sajal2.stars@gmail.com>
2022-06-04 13:33:27 +0600

    student form

--- resources/views/backend/student/edit.blade.php
+++ resources/views/backend/student/edit.blade.php
@@ -15,7 +15,6 @@
   <div class="card p-4">
       @if (isset($information->id))
           
-      
     <form action="{{ route('dashboard.student.information.update',$information->id) }}" method="POST" enctype="multipart/form-data">
         @csrf
         <div class="form-layout row">
@@ -94,7 +93,82 @@
           </div>
     </form>
     @else
-    Nai
+    <form action="{{ route('dashboard.student.information.update') }}" method="POST" enctype="multipart/form-data">
+        @csrf
+        <div class="form-layout row">
+            <div class="form-group col-sm-6">
+                <label class="form-control-label">Father Name:</label>
+                <input class="form-control" type="text" name="fathername" placeholder="Father Name">
+                @error('section_description')
+                    <div class="alert alert-danger">{{ $message }}</div>
+                @enderror
+            </div>
+            <div class="form-group col-sm-6">
+                <label class="form-control-label">Birth Day:</label>
+               <input type="text"  name="birthday" class="form-control">
+                @error('birthday')
+                    <div class="alert alert-danger">{{ $message }}</div>
+                @enderror
+            </div>
+            <div class="form-group col-sm-6">
+                <label class="form-control-label">Gender:</label>
+                <select class="form-select form-control" name="gender" >
+                  <option disabled>-Select One-</option>
+                  <option value="Male">Male</option>
+                  <option value="Female">Female</option>
+                </select>
+                @error('gender')
+                    <div class="alert alert-danger">{{ $message }}</div>
+                @enderror
+            </div>
+            <div class="form-group col-sm-6">
+                <label class="form-control-label">Whatsapp Number:</label>
+                <input type="number" name="mobile" class="form-control" placeholder="Mobile Number">
+                @error('mobile')
+                    <div class="alert alert-danger">{{ $message }}</div>
+                @enderror
+            </div>
+            <div class="form-group col-sm-6">
+                <label class="form-control-label">Address:</label>
+                <input type="text" name="address" class="form-control" placeholder="Address">
+                @error('address')
+                    <div class="alert alert-danger">{{ $message }}</div>
+                @enderror
+            </div>
+            <div class="form-group col-sm-6">
+                <label class="form-control-label">Nationality:</label>
+                <input type="text" class="form-control" placeholder="Nationality"  name="nationality">
+                @error('nationality')
+                    <div class="alert alert-danger">{{ $message }}</div>
+                @enderror
+            </div>
+            <div class="form-group col-sm-6">
+                <label class="form-control-label">Guardian Number:</label>
+                <input type="text" name="guardianname" class="form-control"  placeholder="Guardian Name">
+                @error('guardianname')
+                    <div class="alert alert-danger">{{ $message }}</div>
+                @enderror
+            </div>
+            <div class="form-group col-sm-6">
+                <label class="form-control-label">Guardian Number:</label>
+                <input type="number" name="gnumber" class="form-control" placeholder="Guardian Phone Number">
+                @error('gnumber')
+                    <div class="alert alert-danger">{{ $message }}</div>
+                @enderror
+            </div>
+            <div class="form-group col-sm-6">
+                <label class="form-control-label">Profile Image:</label>
+                <input class="form-control" type="file" name="profile_photo">
+                @error('profile_photo')
+                    <div class="alert alert-danger">{{ $message }}</div>
+                @enderror
+              </div>
+  
+            <div class="form-layout-footer col-12">
+              <button class="btn btn-info mg-r-5">Uplode</button>
+            </div>
+          </div>
+    </form>
     @endif
   </div> 
 </div>
