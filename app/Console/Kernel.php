209c1a830a3d2b0009b1ca7a03b9ee5974d1029a
Sajal Sardar <sajal2.stars@gmail.com>
2022-06-21 12:46:07 +0600

    cron job

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -5,17 +5,17 @@ namespace App\Console;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
-class Kernel extends ConsoleKernel
-{
+class Kernel extends ConsoleKernel {
     /**
      * Define the application's command schedule.
      *
      * @param  \Illuminate\Console\Scheduling\Schedule  $schedule
      * @return void
      */
-    protected function schedule(Schedule $schedule)
-    {
-        // $schedule->command('inspire')->hourly();
+    protected function schedule(Schedule $schedule) {
+        $schedule->command('queue:work', [
+            '--max-time' => 300,
+        ])->withoutOverlapping();
     }
 
     /**
@@ -23,9 +23,8 @@ class Kernel extends ConsoleKernel
      *
      * @return void
      */
-    protected function commands()
-    {
-        $this->load(__DIR__.'/Commands');
+    protected function commands() {
+        $this->load(__DIR__ . '/Commands');
 
         require base_path('routes/console.php');
     }
