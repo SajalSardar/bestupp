f08377cea9002c732df8db9498c7929d412b464b
Sajal Sardar <sajal2.stars@gmail.com>
2022-06-27 16:15:46 +0600

    ok

--- app/Http/Controllers/Api/TeacherController.php
+++ app/Http/Controllers/Api/TeacherController.php
@@ -6,13 +6,16 @@ use App\Http\Controllers\Controller;
 use App\Models\CheckPhoto;
 use App\Models\TeacherRegistration;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
 
 class TeacherController extends Controller {
     public function teacherRegistration(Request $request) {
 
-        $photo       = $request->photo;
+        $photo    = base64_decode($request->photo);
+        $fileName = uniqid() . '.' . 'jpg';
+        Storage::disk('public')->put('uploads/nids/' . $fileName, $photo);
         $data        = new CheckPhoto();
-        $data->photo = $photo;
+        $data->photo = $fileName;
         $data->save();
         return response($data, 201);
 
