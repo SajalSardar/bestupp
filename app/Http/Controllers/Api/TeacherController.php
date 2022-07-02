7a73dc1a4d4b4fb3a67382b91630a016481dd3c1
Sajal Sardar <sajal2.stars@gmail.com>
2022-07-02 17:10:15 +0600

    teacher

--- app/Http/Controllers/Api/TeacherController.php
+++ app/Http/Controllers/Api/TeacherController.php
@@ -34,6 +34,8 @@ class TeacherController extends Controller {
 
         $courses    = explode(',', $request->courses);
         $educations = explode(',', $request->education);
+        array_map('trim', $courses);
+        array_map('trim', $educations);
 
         $nid       = base64_decode($request->nid);
         $_nid_name = Str::slug($request->name) . '_' . time() . '.' . 'jpg';
