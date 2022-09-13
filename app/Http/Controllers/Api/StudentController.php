0085d2fa58f9a87dca2f516a8016d777e03b8622
Sajal Sardar <sajal2.stars@gmail.com>
2022-09-13 16:57:29 +0600

    code change

--- app/Http/Controllers/Api/StudentController.php
+++ app/Http/Controllers/Api/StudentController.php
@@ -9,6 +9,7 @@ use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
 
 class StudentController extends Controller {
     public function studentRegistration(Request $request) {
